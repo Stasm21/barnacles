272e8632dfbe1fc06437da586093964aef54fd6a
Stas <stasm21@gmail.com>
2022-01-28 16:05:54 -0800

    navbar finished with added img

--- src/App.js
+++ src/App.js
@@ -1,19 +1,11 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
-// import Home from "./components/Home";
-// import Cook from "./components/Cook";
-// import { Switch, Route } from "react-router-dom";
 
 
 function App() {
     return (
         <div className="App">
-            <h1 className="header">Oregon Gooseneck Barnacles</h1>
             <Navbar />
-            {/* <Switch>
-                <Route path="/" exact render={() => <Home />} />
-                <Route path="/cook" render={() => <Cook />} />
-            </Switch> */}
         </div>
     );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,29 +1,46 @@
 import React from "react";
 import "./Navbar.css";
+import black_white_barnacles from '../img/black_white_barnacles.png';
 // import Cook from "./Cook";
 // import { Link } from "react-router-dom";
 
 const Navbar = () => {
     return (
-        <div className="navbar">
-            <ul className="nav-list">
-                {/* <Link to="/">
+        <div className="flexbox-container">
+            <div className="title">
+                <h1 className="header">
+                    OREGON GOOSENECK BARNACLES{" "}
+                    <span>
+                        <img
+                            className="img"
+                            src={black_white_barnacles}
+                            alt="barnacles"
+                        />
+                    </span>
+                </h1>
+            </div>
+            <div className="img">
+                {/* <img className="img" src={black_white_barnacles} alt="barnacles" /> */}
+            </div>
+            <div className="items">
+                <ul>
                     <li>
-                        <a>
-                            <Home />
-                            Home
-                        </a>
+                        <a href="#">HOME</a>
                     </li>
-                </Link>
-                <Link to="/cook">
                     <li>
-                        <a>
-                            <Cook />
-                            Home
-                        </a>
+                        <a href="#">HARVEST</a>
                     </li>
-                </Link> */}
-            </ul>
+                    <li>
+                        <a href="#">COOK</a>
+                    </li>
+                    <li>
+                        <a href="#">REGULATIONS</a>
+                    </li>
+                    <li>
+                        <a href="#">FAQS</a>
+                    </li>
+                </ul>
+            </div>
         </div>
     );
 };
