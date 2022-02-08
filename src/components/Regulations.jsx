916a7e90ff11e2aad63938c5352649e5d220d2b1
Stas <stasm21@gmail.com>
2022-02-07 19:14:53 -0800

    regulations page rought draft finished

--- src/components/Regulations.jsx
+++ src/components/Regulations.jsx
@@ -1,11 +1,33 @@
-import './Regulations'
+import "./Regulations.css";
+import jetty from "../img/or_jetty.jpeg";
 
 const Regulations = () => {
     return (
         <div>
-            Regulations
+            <div className="reg-head-container">
+                <h1 className="regulations-heading">Oregon Regulations</h1>
+            </div>
+            <div className="reg-info">
+                <p>
+                    Currently Oregon regulations limit wild harvest of the
+                    barnacles to 50 individuals of any size on man-made
+                    structures like rock jetties but not on natural rock
+                    formations. Please make sure to check regulations on Oregons
+                    Department of Fish and Wildfire website at
+                    <a
+                        className="reg-site"
+                        href="https://myodfw.com/"
+                        target="_blank"
+                    >
+                        <strong> myODFW</strong>
+                    </a>
+                </p>
+            </div>
+            <div>
+                <img src={jetty} alt="oregon jetty on coast" className="img-box" />
+            </div>
         </div>
     );
-}
+};
 
 export default Regulations;
